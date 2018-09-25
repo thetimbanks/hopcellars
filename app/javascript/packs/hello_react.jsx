f77432cf2656d89a17a63976236998996571e812
Tim Banks <thetimbanks@gmail.com>
2018-09-25 18:32:22 -0500

    Add react and react-bootstrap dependecies

---
+++ app/javascript/packs/hello_react.jsx
@@ -0,0 +1,37 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import Button from 'react-bootstrap/lib/Button';
+
+const Hello = props => (
+  <Jumbotron>
+    <h1>Hello, {props.name}!</h1>
+    <p>
+      This is a simple hero unit, a simple jumbotron-style component for calling
+      extra attention to featured content or information.
+    </p>
+    <p>
+      <Button variant="primary">Learn more</Button>
+    </p>
+  </Jumbotron>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: PropTypes.string
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
